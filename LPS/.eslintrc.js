6b2ee47732c275f3bed9efc2384fef16ed659dbb
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-16 17:36:55 +0900

    挫折

--- LPS/.eslintrc.js
+++ LPS/.eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
-    rules: {
-        quotes: ['error', 'double'
-		],
-	},
-}
+    root: true,
+    env: {
+        es2020: true,
+        node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020,
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.eslint.json'],
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    rules: {},
+};
