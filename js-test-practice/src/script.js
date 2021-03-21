34918c900650ad643c52a0a91480e70535e537b1
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-21 18:09:13 +0900

    ブログ用 調整

--- js-test-practice/src/script.js
+++ js-test-practice/src/script.js
@@ -1,9 +1,7 @@
-
-document.getElementById('alert').addEventListener('click', function (){
-  alert("document.getElementByIdのalert");
-});;
-
-$('#alert').on('click', function () {
-  console.log('jQueryのalert');
-  alert('jQueryのalert');
+document.getElementById('box').addEventListener('click', function () {
+	alert('This is box alert')
 });
+
+const test = 'This is test';
+
+console.log(test)
